e3f2b85c4f3ae2f50b3325379f422bc8713c83c0
emmajane <emma@emmajane.net>
2013-11-27 21:29:42 -0500

    Lesson 15 completed. page fragment tested in a browser for basic layout, page.tpl.php has new variables added, corrections to SCSS layout class names

---
+++ domicile/templates/page.tpl.php
@@ -0,0 +1,34 @@
+<div class="page fullwidth">
+  <div id="banner" class="fullwidth"> banner </div>
+  <div id="main" class="fullwidth clearfix">
+    <div id="nav" class="sidebar--nav"> 
+ <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+<IMG></a>
+
+    <?php print render($page['nav']); ?>
+
+    </div>
+    <div id="feature" class="sidebar--feature">
+      <div class="decoration">Big plate</div>
+      <?php print render($page['featured']); ?>
+      <div class="decoration">Small plate</div>
+    </div>
+    <div id="content" class="main-content">
+
+         <?php print $messages; ?>
+         <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
+<a id="main-content"></a>
+         <?php print render($title_prefix); ?>
+         <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+         <?php print render($title_suffix); ?>
+         <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+         <?php print render($page['help']); ?>
+         <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+         <?php print render($page['content']); ?>
+         <?php print $feed_icons; ?> 
+
+    </div>
+  </div>
+
+  <div id="copyright" class="fullwidth clearfix"> <?php print render($page['copyright']); ?> </div>
+</div>
